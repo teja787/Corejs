9b10747968a2a356549b8129ab939c7e3c5059cb
teja787 <tejafeb1898@gmail.com>
2021-03-11 21:00:23 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// 1. add multiple strings and number
+// ex: time is 10:30 pm
+// print 100 random numbers
+// between 0 to 10
+// if Number is less than 5 store in array "minors" otherwise "major"
+
+const a = "time";
+const b = "10:30";
+const c = "PM";
+const d = a + b + c;
+console.log(d);
+
+console.log("---------------1 done-----------------");
+
+const minor = [];
+const major = [];
+// if Random-number is <5 print minr number; else major number
+
+// i++ = (i = i+1)
+let random 
+for (let i = 0; i < 100; i++) {
+  console.log(Math.random() * 10);
+}
+
+// if (<5){
+//     console.log(minor)
+// }
