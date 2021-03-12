f7f62a8995fabb3fc23e0aab2ca7b874c0eb18ee
teja787 <tejafeb1898@gmail.com>
2021-03-12 21:00:12 +0530

    aftre redoing

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ module.exports = {
   numopr: () => {
     const minor = [];
     const major = [];
-    for (let i = 0; i < 100; i++) {
+    for (i = 0; i < 100; i++) {
       let random = Math.round(Math.random() * 10);
       if (random < 5) {
         minor.push(random);
@@ -10,18 +10,17 @@ module.exports = {
         major.push(random);
       }
     }
-
-    console.log("All minors b/w 100 is ", minor);
-    console.log("All majors b/w 100 is ", major);
-    return "Hello";
+    console.log("minor num", minor);
+    console.log("major num", major);
+    return "hai";
   },
+
   stropr: () => {
     const a = "time";
-    const b = "10:30";
+    const b = "8:50";
     const c = "PM";
     const d = a + b + c;
     console.log(d);
-    console.log("---------------1 done-----------------");
-    return "World";
+    return "hello";
   },
 };
