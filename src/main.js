5b059feb4da058a39c7006d1ea21f169fb17442b
teja787 <tejafeb1898@gmail.com>
2021-03-12 21:14:14 +0530

    arr to str

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-const utils = require("./utils");
+const introDuction =
+  "Another_handy_React_animation_library_to_have_in_your_tool_belt";
+const arr = introDuction.split("_");
+console.log(arr);
+console.log(typeof arr);
+console.log(Array.isArray(arr));
 
-utils.stropr();
-utils.numopr();
+console.log(arr.join(" "));
+
+/**
+ * string, number , boolean
+ * all are Objects(array, function)
+ */
