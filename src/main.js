5b117fb0b193cbf07bb8caa9f4d8cefcd1ef687e
teja787 <tejafeb1898@gmail.com>
2021-03-13 20:45:42 +0530

    1. Asynchonous javascript calls

--- src/main.js
+++ src/main.js
@@ -1,56 +1,16 @@
-// const abcObj = {
-//   a: 1,
-//   b: 2,
-//   c: 3,
-// };
+const tableOfTen = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-const arr1 = ["a", "b", "c"];
-const arr2 = [1, 2, 3];
+const fetchTable = new Promise((res, reject) => {
+  setTimeout(() => {
+    res(tableOfTen);
+  }, 0);
+});
 
-// const obj = arr1.reduce((ab, arr1, i) => {
-//   ab[arr1] = arr2[i];
-//   return ab;
-// }, {});
-// console.log(obj);
-
-// const combinedObj = {};
-// arr1.forEach((el, index) => {
-//   combinedObj[el] = arr2[index];
-// });
-// console.log(combinedObj);
-
-const combinedObj2 = {};
-
-for (let i = 0; i < arr1.length; i++) {
-  console.log(arr1[i]);
-  console.log(arr2[i]);
-  combinedObj2[arr1[i]] = arr2[i];
-}
-console.log(combinedObj2);
-
-console.log("-----------Task one completed-----------");
-// console.log(Object.keys(abcObj));
-// console.log(Object.values(abcObj));
-
-// you have to print table of 10 .
-// each value per second
-
-// sec1 => 10
-// sec2 => 20
-
-// const numbers = () => {
-//   for (let i = 1; i <= 10; i++) {
-//     setTimeout(() => {
-//       console.log(i);
-//     }, i * 1000);
-//   }
-// };
-// numbers();
-
-let count = 0;
-
-const cancelInterval = () => clearInterval(intervalRef);
-const intervalRef = setInterval(() => {
-  console.log(++count);
-  if (count == 10) cancelInterval();
-}, 1000);
+console.log("before table");
+fetchTable.then((res) => {
+  console.log(res);
+});
+setTimeout(() => {
+  console.log("Hello world");
+}, 0);
+console.log("after table");
