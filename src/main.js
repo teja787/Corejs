f6675eed182bf849c923d98d7f0da73d869b81d8
teja787 <tejafeb1898@gmail.com>
2021-03-23 20:29:24 +0530

    1. Accordion completed

--- src/main.js
+++ src/main.js
@@ -1,94 +1,89 @@
-const head1 = document.querySelector("#head1");
-console.log(head1);
-head1.style.listStyleType = "none";
-head1.style.margin = 0;
-head1.style.padding = 0;
-head1.style.overflow = "hidden";
-head1.style.backgroundColor = "#333";
+const createDiv = () => document.createElement("div");
 
-const list1 = document.querySelector("#list1");
-console.log(list1);
-list1.style.float = "left";
+const createTxtNode = (content) => document.createTextNode(content);
 
-const list2 = document.querySelector("#list2");
-list2.style.float = "left";
-list2.style.display = "block";
-list2.style.color = "white";
-list2.style.textAlign = "center";
-list2.style.padding = "25px";
-list2.style.textDecoration = "none";
+const setStyles = (node, properties) => {
+  for (const key in properties) {
+    if (Object.hasOwnProperty.call(properties, key)) {
+      const element = properties[key];
+      node.style[key] = element;
+    }
+  }
+};
 
-const list3 = document.querySelector("#list3");
-list3.style.float = "left";
-list3.style.display = "block";
-list3.style.color = "white";
-list3.style.textAlign = "center";
-list3.style.padding = "25px";
-list3.style.textDecoration = "none";
+const createAcordion = (header, content) => {
+  // creating wrapper and adding styles to it
+  const wrap = createDiv();
+  let isContentHidden = true;
+  setStyles(wrap, {
+    border: "1px solid gray",
+    backgroundColor: "white",
+  });
+  const head = createDiv();
+  //   creating header and adding style to it
+  setStyles(head, {
+    padding: "5px",
+    borderBottom: "1px solid gray",
+    display: "flex",
+    justifyContent: "space-between",
+  });
+  //   creating header text as a child to wrapper
+  const headerTxt = createTxtNode(header);
+  head.appendChild(headerTxt);
+  //   creating close icon in header
+  const closeIcon = createDiv();
+  setStyles(closeIcon, { cursor: "pointer" });
+  closeIcon.onclick = () => {
+    isContentHidden = !isContentHidden;
+    setStyles(contentDiv, {
+      display: isContentHidden ? "none" : "block",
+    });
+  };
+  const closeIconTxt = createTxtNode("X");
+  closeIcon.appendChild(closeIconTxt);
+  head.appendChild(closeIcon);
+  //   creating content and adding styles to it
+  const contentDiv = createDiv();
+  setStyles(contentDiv, {
+    display: "none",
+    padding: "5px",
+  });
+  //   creating content text as wrapper child
+  const contentTxt = createTxtNode(content);
+  contentDiv.appendChild(contentTxt);
+  wrap.appendChild(head);
+  wrap.appendChild(contentDiv);
+  //   wraper getting assingned to main as child
+  document.querySelector("#acord").appendChild(wrap);
+};
+// calling create accodion method
+// createAcordion("header1", "body1");
 
-const list4 = document.querySelector("#list4");
-list4.style.float = "left";
-list4.style.display = "block";
-list4.style.color = "white";
-list4.style.textAlign = "left";
-list4.style.padding = "25px";
-list4.style.textDecoration = "none";
+/**
+ * using map on accordion
+ *
+ * v =[
+ * [header1, body1],
+ * [header1, body1],
+ * [header1, body1],
+ * [header1, body1],5
+ * ]
+ */
 
-const list5 = document.querySelector("#list5");
-list5.style.float = "left";
-list5.style.display = "block";
-list5.style.color = "white";
-list5.style.textAlign = "center";
-list5.style.padding = "25px";
-list5.style.textDecoration = "none";
+const accordion = [
+  ["header1", "body1"],
+  ["header2", "body2"],
+  ["header3", "body3"],
+  ["header4", "body4"],
+  ["header5", "body5"],
+];
 
-function mouseOver(id) {
-  document.getElementById(id).style.color = "green";
-}
-
-function mouseOut(id) {
-  document.getElementById(id).style.color = "white";
-}
-// setTimeout(() => {
-//   const listContainer = document.querySelector("#listContainer");
-//   for (let i = 0; i <= 100; i++) {
-//     const li = document.createElement("li");
-//     const txtNode = document.createTextNode(i + 1);
-//     li.appendChild(txtNode);
-//     listContainer.appendChild(li);
-//     console.log(i);
-//   }
-// }, 0);
-
-function popUp() {
-  const popupContainer = document.querySelector("#popupContainer");
-  popupContainer.style.display = "flex";
-  //   setTimeout(() => {
-  //     popupContainer.style.display = "none";
-  //   }, 1000);
-}
-function popUpClose() {
-  const popupContainer = document.querySelector("#popupContainer");
-  popupContainer.style.display = "none";
-}
-
-function openNav() {
-  document.getElementById("sideNav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("sideNav").style.width = "0";
-}
-
-function getConfirmation() {
-  var modal = confirm("Do you want to continue ?");
-  const logger = document.querySelector("#logger");
-  logger.innerHTML = "";
-  const div = document.createElement("div");
-  const txtNode = document.createTextNode(
-    modal == true ? "user want to continue" : "user do not want to continue"
-  );
-  div.appendChild(txtNode);
-  logger.appendChild(div);
-}
+const arr = accordion.map((arrItem) => {
+  createAcordion(...arrItem);
+  /**
+   * why this is working .
+   * Learn about spread operators in js.
+   * Learn about destructuring (array and obj) in js.
+   * Each with atleast 5 examples
+   */
+});
