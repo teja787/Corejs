4df20a17b033e77328e9b7f70916a1e521883077
teja787 <tejafeb1898@gmail.com>
2021-04-01 21:17:36 +0530

    temp

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-// 1.  Boolean in js
-// 2.  if else and switch statements
-// 3.  lopps (forLoop, forEach, forin, forof)
+// for (let index = 1; index < 10; index++) {
+//   console.log(index * 16);
+// }
 
-// 1. 0 is false and other true
+const scores = [2, 4, 6, 7, 8, 9];
 
-function isJohn(name) {
-  if (name === "john") return true;
-  else return false;
+// for (let index = 0; index < scores.length; index++) {
+//   scores[index] = 5;
+// }
+
+// console.log(scores);
+
+// 2. forEach
+
+// for (let index = 0; index < scores.length; index++) {
+//   console.log(scores[index]);
+// }
+
+// const printItems = (item) => {
+//   console.log(item);
+// };
+
+// scores.forEach(printItems);
+
+for (const iterator of scores) {
+  console.log(iterator);
 }
-
-console.log(isJohn("john"));
