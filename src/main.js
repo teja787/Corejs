4898bf21b4ccfabfa617c1cffb957ee60f23ba99
teja787 <tejafeb1898@gmail.com>
2021-04-02 21:05:24 +0530

    1. run time datatypes

--- src/main.js
+++ src/main.js
@@ -1,27 +1,45 @@
 // for (let index = 1; index < 10; index++) {
 //   console.log(index * 16);
 // }
+/**
+ * 1. filter prime number from this array and print
+ * 2. filter even number from this array and print
+ * 3. filter odd number from this array and print
+ * 4. print sum  of all items in array.
+ * 5. [1 to 4] do with for, forEach, forof
+ */
+// const utils = require("./utils");
 
-const scores = [2, 4, 6, 7, 8, 9];
+// console.log(utils);
+// utils.filterPrimes("Hello world");
+// utils.filterEvenOddUsingFor();
 
-// for (let index = 0; index < scores.length; index++) {
-//   scores[index] = 5;
+// Array or Object
+// [number, string, Boolean] other than these. all are object in js
+
+const scores = [2, 4, 6, 7, 8, 9, 11, 13, 18, 20, 30];
+// console.log(typeof scores);
+// console.log(Array.isArray(scores));
+
+// [number,string, object, Boolean, Array]
+// {}
+
+// function checkArgumentType(inputValue) {
+//   if (typeof inputValue == "number") {
+//     console.log("it is a number");
+//   } else if (typeof inputValue == "string") {
+//     console.log("it is a string");
+//   } else if (typeof inputValue == "boolean") {
+//     console.log("it is a boolean");
+//   } else if (Array.isArray(inputValue)) {
+//     console.log("it is an array");
+//   } else if (typeof inputValue == "object") {
+//     console.log("it is an object");
+//   }
 // }
 
-// console.log(scores);
-
-// 2. forEach
-
-// for (let index = 0; index < scores.length; index++) {
-//   console.log(scores[index]);
-// }
-
-// const printItems = (item) => {
-//   console.log(item);
-// };
-
-// scores.forEach(printItems);
-
-for (const iterator of scores) {
-  console.log(iterator);
-}
+// checkArgumentType(10);
+// checkArgumentType("hello");
+// checkArgumentType(true);
+// checkArgumentType([]);
+// checkArgumentType({});
--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,90 @@
-module.exports = {
-  numopr: () => {
-    const minor = [];
-    const major = [];
-    for (i = 0; i < 100; i++) {
-      let random = Math.round(Math.random() * 10);
-      if (random < 5) {
-        minor.push(random);
-      } else {
-        major.push(random);
-      }
-    }
-    console.log("minor num", minor);
-    console.log("major num", major);
-    return "hai";
-  },
+const scores = [2, 4, 6, 7, 8, 9, 11, 13, 18, 20, 30];
 
-  stropr: () => {
-    const a = "time";
-    const b = "8:50";
-    const c = "PM";
-    const d = a + b + c;
-    console.log(d);
-    return "hello";
-  },
+function filterPrimes(message) {
+  console.log(message);
+  const prime = scores.filter((number) => {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) return false;
+    }
+    return true;
+  });
+
+  console.log(prime);
+}
+
+function filterEvenOddUsingFor() {
+  let even = [];
+  let odd = [];
+
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] % 2 == 0) {
+      even.push(scores[i]);
+    } else {
+      odd.push(scores[i]);
+    }
+  }
+  console.log(even);
+  console.log(odd);
+}
+// filtering even and odd numbers using forof
+function filterEvenOddUsingForOf() {
+  const a = [];
+  const b = [];
+  for (const iterator of scores) {
+    if (iterator % 2 == 0) {
+      a.push(iterator);
+    } else {
+      b.push(iterator);
+    }
+  }
+  console.log(a);
+  console.log(b);
+}
+// filtering even and odd numbers using foreach
+function filterEvenOddUsingForEach() {
+  const c = [];
+  const d = [];
+  scores.forEach((element) => {
+    if (element % 2 == 0) {
+      c.push(element);
+    } else {
+      d.push(element);
+    }
+  });
+  console.log(c);
+  console.log(d);
+}
+// sum of array items using for
+function sumUsingFor() {
+  let sum = 0;
+  for (let i = 0; i < scores.length; i++) {
+    sum = sum + scores[i];
+  }
+  console.log(sum);
+}
+//sum of array items using foreach
+function sumUsingForEach() {
+  let s = 0;
+  scores.forEach((element) => {
+    s = s + element;
+  });
+  console.log(s);
+}
+//sum of array items using forof
+function sumUsingForOf() {
+  let add = null;
+  for (const iterator of scores) {
+    add = add + iterator;
+  }
+  console.log(add);
+}
+
+module.exports = {
+  sumUsingForOf: sumUsingForOf,
+  sumUsingForEach: sumUsingForEach,
+  sumUsingFor: sumUsingFor,
+  filterEvenOddUsingForEach: filterEvenOddUsingForEach,
+  filterEvenOddUsingForOf: filterEvenOddUsingForOf,
+  filterEvenOddUsingFor: filterEvenOddUsingFor,
+  filterPrimes: filterPrimes,
 };
